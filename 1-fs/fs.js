cab3800bc398d6587360f55ffe42aab9bf8c0014
Dave <dlrobjr@gmail.com>
2023-12-06 10:49:59 -0700

    Messing with imports and cleaning up code

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -1,101 +1,102 @@
 #!/usr/bin/env node
-const {exit} = require('node:process')
+const {exit, argv} = require('node:process');
+const {log, error} = require('node:console');
 const fs = require('fs');
-const selection = process.argv[2];
-//console.log(process.argv);
+const selection = argv[2];
 
 if(!selection){
-    console.error("Usage: node fs.js [read | create | update | destroy]")
+    error("Usage: node fs.js [read | create | update | destroy]")
     exit(1);
 }
-let pets = [];
-fs.readFile("../pets.json", "utf-8", function(error, data){
-    if(error){
-        console.log(error);
-        exit(1);
-    }
-     pets = JSON.parse(data);
-})
 
-    
+let pets = [];
+
+function loadContent(){
+    fs.readFile("../pets.json", "utf-8", function(err, data){
+    if(err){
+        error(err);
+        exit(1);
+    } else{
+        pets = JSON.parse(data);
+    }
+     
+     switch(selection.toLowerCase()){
+        case 'read': read();
+                    break;
+        case 'update': update();
+                    break;
+        case 'create': create();
+                    break;
+        case 'destroy': destroy();
+                    break;
+        default: error("Usage: node fs.js [read | create | update | destroy]")
+        exit(1);
+     }
+})
+}
 
 function read() {
-        const index = process.argv[3];
+        const index = argv[3];
         if (!index){
-            console.log(pets);
+            log(pets);
         } else if (index < 0 || index >=pets.length || !Number.isInteger(Number(index))){
-            console.error("Usage: node fs.js read INDEX")
+            error("Usage: node fs.js read INDEX")
             exit(1);
         } else if(index < pets.length){
-            console.log(pets[index])
-        } 
-    
+            log(pets[index])
+        }  
 }
 
 function create() {
-    const age = process.argv[3];
-    const kind = process.argv[4];
-    const name = process.argv[5];
+    const age = argv[3];
+    const kind = argv[4];
+    const name = argv[5];
 
     if(!name || !Number.isInteger(Number(age))){
-        console.error("Usage: node fs.js create AGE KIND NAME");
+        error("Usage: node fs.js create AGE KIND NAME");
     } else{
-        newPet = {
+        const newPet = {
             "age": Number(age),
             "kind": kind,
             "name": name,
         }
         pets.push(newPet);
-        fs.writeFile("../pets.json", JSON.stringify(pets), function(error){
-            error ? console(error) : console.log(newPet);
+        fs.writeFile("../pets.json", JSON.stringify(pets), function(err){
+            err ? error(err) : log(newPet);
         })
     }
-
 }
 
 function update() {
-    const index = process.argv[3];
-    const age = process.argv[4];
-    const kind = process.argv[5];
-    const name = process.argv[6];
+    const index = argv[3];
+    const age = argv[4];
+    const kind = argv[5];
+    const name = argv[6];
 
     if(index <0 || !name || !Number.isInteger(Number(index)) || !Number.isInteger(Number(age))){
-        console.error("Usage: node fs.js update INDEX AGE KIND NAME");
+        error("Usage: node fs.js update INDEX AGE KIND NAME");
     } else {
         pets[index]["age"] = Number(age);
         pets[index]["kind"] = kind;
         pets[index]["name"] = name;
-        fs.writeFile("../pets.json", JSON.stringify(pets), function(error){
-            error ? console(error) : console.log(pets[index]);
+        fs.writeFile("../pets.json", JSON.stringify(pets), function(err){
+            err ? error(err) : log(pets[index]);
         })
     }
 }
 
 function destroy() {
-    const index = process.argv[3];
+    const index = argv[3];
 
     if (index < 0 || index >=pets.length || !Number.isInteger(Number(index))){
-        console.error("Usage: node fs.js destroy INDEX")
+        error("Usage: node fs.js destroy INDEX")
         exit(1);
     } else {
         let oldPet = pets.splice(index, 1);
-        fs.writeFile("../pets.json", JSON.stringify(pets), function(error){
-            error ? console(error) : console.log(oldPet[0]);
+        fs.writeFile("../pets.json", JSON.stringify(pets), function(err){
+            err ? error(err) : log(oldPet[0]);
         })
     }
 }
 
-setTimeout(function(){
-    switch(selection.toLowerCase()){
-        case 'read': read();
-                    break;
-        case 'update': update();
-                    break;
-        case 'create': read();
-                    break;
-        case 'destroy': update();
-                    break;
-        default: console.error("Usage: node fs.js [read | create | update | destroy]")
-        exit(1);
-    }
-}, 1000);
+loadContent();
